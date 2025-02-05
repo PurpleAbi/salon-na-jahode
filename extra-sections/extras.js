e4930fb756afb3960fbdb51cc12c82040dbf9063
PurpleAbi <abigail.santana.medina@gmail.com>
2025-02-05 23:59:18 +0100

    kontakt section fix and other small fixes

--- extra-sections/extras.js
+++ extra-sections/extras.js
@@ -1,42 +1,16 @@
-// let navItems = $(".nav-a");
+let navItems = $(".nav-a");
+let title = $("title").html();
 
-// // console.log(navItems);
+$("body").ready(()=>{
+    highlightActiveNavItem();
+});
 
-// $("body").ready(()=>{
-//     getNavItem();
-// });
+function highlightActiveNavItem(){
+    for (const item of navItems){ 
+        let itemsContent = item.innerHTML;
+        if (itemsContent == title){
+            return item.classList.add("active");
+        }
+    }
+}
 
-// let navA = $("#oteviraci-doba").html();
-
-// function getNavItem(){
-//     let title = $("title").html();
-//     if (title == navA){
-//         return $("#oteviraci-doba").addClass("active");
-//     }
-// }
-
-// for (const item of navItems){
-//     let itemIndex = navItems[i];
-//     if (item == title){
-
-//     }
-// }
-// let acc = document.getElementsByClassName("accordion-button");
-// let i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     let body = this.nextElementSibling;
-//     if (body.style.display === "block") {
-//       body.style.display = "none";
-//     } else {
-//       body.style.display = "block";
-//     }
-//     if (body.style.maxHeight) {
-//       body.style.maxHeight = null;
-//     } else {
-//       body.style.maxHeight = body.scrollHeight + "px";
-//     }
-//   });
-// }

c646ffd346aa4dab7850e9d9f190f434a424c07e
PurpleAbi <abigail.santana.medina@gmail.com>
2025-02-07 19:38:52 +0100

    minor fixes all and footer more

---
+++ main.js
@@ -0,0 +1,4 @@
+function redirectToKontaktujteNas() {
+    window.location.href = 'extra-sections/kontaktujete-nas.html';
+}
+document.getElementById('contactButton').onclick = redirectToKontaktujteNas;
